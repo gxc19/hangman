83eb570aa0fe9bb299ef4593e442a40c717811b5
Ga Ming Chan <gmchan19@gmail.com>
2023-10-25 19:08:14 +0100

    added pages

---
+++ src/pages/Category.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Category.css";
+
+const Category = () => {
+  return (
+    <div>
+      <h1>Pick a Topic</h1>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/pages/GameMode.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/GameMode.css";
+
+class GameMode extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Game Time</h1>
+      </div>
+    );
+  }
+}
+
+export default GameMode;
